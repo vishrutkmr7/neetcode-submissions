644e64c04aaa4e1f0320d0270dd4332bb51922f2
Vishrut Jha <vishrutkmr7@gmail.com>
2026-06-26 01:52:35 -0700

    Add: implement-stack-using-queues - submission-1

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-1.swift
@@ -0,0 +1,37 @@
+class MyStack {
+
+    var queue: [Int] = []
+
+    init() {
+
+    }
+
+    func push(_ x: Int) {
+        queue.append(x)
+        // rotate
+        for i in 0 ..< queue.count - 1 {
+            queue.append(queue.removeFirst())
+        }
+    }
+
+    func pop() -> Int {
+        return queue.removeFirst()
+    }
+
+    func top() -> Int {
+        return queue.first!
+    }
+
+    func empty() -> Bool {
+        return queue.isEmpty
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * let obj = MyStack()
+ * obj.push(x)
+ * let param_2 = obj.pop()
+ * let param_3 = obj.top()
+ * let param_4 = obj.empty()
+ */
