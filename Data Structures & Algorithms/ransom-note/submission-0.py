85f14581b13272fa96c0789a7a8d367abd7b0833
Vishrut Jha <vishrutkmr7@gmail.com>
2026-07-16 20:02:06 -0700

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for letter in ransomNote:
+            if letter in magazine:
+                magazine = magazine.replace(letter, "", 1)
+            else:
+                return False
+        # print(magazine)
+        return True
+        
