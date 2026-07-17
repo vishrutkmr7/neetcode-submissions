1ecdad97080a22344f8a9e830ef35d24b5c23ae9
Vishrut Jha <vishrutkmr7@gmail.com>
2026-07-16 21:15:51 -0700

    Add: find-anagram-mappings - submission-0

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        positionMap = {}
+        for index, num in enumerate(nums2):
+            if num in positionMap.keys():
+                positionMap[num].append(index)
+            else:
+                positionMap[num] = [index]
+        
+        res = []
+        
+        for num in nums1:
+            if len(positionMap[num]) > 1:
+                res.append(positionMap[num].pop())
+            else:
+                res.append(positionMap[num][0])
+        
+        return res
+        
